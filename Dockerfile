5086f4c21fbc77d5693150f3f37a1fdccd7c0acf
Ahmad Azuddin <exectraquasar@gmail.com>
2022-02-18 23:41:10 +0800

    implement paddleocr

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,11 @@ FROM python:3.9
 
 WORKDIR /usr/src/app
 
+RUN apt-get update
+RUN apt-get install ffmpeg libsm6 libxext6  -y
+
+RUN python -m pip install paddlepaddle -i https://mirror.baidu.com/pypi/simple
+
 COPY requirements.txt ./
 RUN pip install --no-cache-dir -r requirements.txt
 
--- app.py
+++ app.py
@@ -1,14 +1,20 @@
-import csv
-import easyocr
+from paddleocr import PaddleOCR,draw_ocr
+# Paddleocr supports Chinese, English, French, German, Korean and Japanese.
+# You can set the parameter `lang` as `ch`, `en`, `fr`, `german`, `korean`, `japan`
+# to switch the language model in order.
+ocr = PaddleOCR(use_angle_cls=True, lang='en') # need to run only once to download and load model into memory
+img_path = '/data/uploaded_files/receipt-sample.jpeg'
+result = ocr.ocr(img_path, cls=True)
 
-file = open('/data/python/result.csv', 'w')
-writer = csv.writer(file)
-writer.writerow(['Result'])
+for line in result:
+  print(line)
 
-IMAGE_PATH = '/data/uploaded_files/receipt-sample.jpeg'
-
-reader = easyocr.Reader(['en'])
-result = reader.readtext(IMAGE_PATH,paragraph="False")
-result
-
-writer.writerow([result])
+# draw result
+from PIL import Image
+image = Image.open(img_path).convert('RGB')
+boxes = [line[0] for line in result]
+texts = [line[1][0] for line in result]
+scores = [line[1][1] for line in result]
+im_show = draw_ocr(image, boxes, texts, scores, font_path='/data/python/fonts/latin.ttf')
+im_show = Image.fromarray(im_show)
+im_show.save('/data/result/receipt-sample.jpg')
